f348ed56106aadc3a8bc6b4cbc61df0cd5e333ab
Stefan Buck <web@broud.de>
2014-10-13 15:48:46 +0200

    inject endpoint in config.js file

---
+++ example/mcap-project/client/config.js
@@ -0,0 +1,8 @@
+  "use strict";
+  angular.module('config',[])
+    .value('baseUrl','hasdf')
+    .value('apiEndpoint','')
+    .value('version','23');
+
+
+    console.log("hier");
---
+++ example/mcap-project/client/js/config.js
@@ -0,0 +1,3 @@
+  "use strict";
+  angular.module('config',[])
+    .value('baseUrl','')
